9fa0988b0bde7e41509533bba01c062bfbe12656
Danil-Nikolaev <danil_nikolaev_96@bk.ru>
2024-04-09 14:48:46 +0300

    add logger in controller

--- producer/src/main/java/com/example/stream/Controller.java
+++ producer/src/main/java/com/example/stream/Controller.java
@@ -2,6 +2,9 @@ package com.example.stream;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -10,9 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/products")
 public class Controller {
 
+    @Autowired
+    private Producer producer;
+    private final Logger LOGGER = LoggerFactory.getLogger(Controller.class); 
     @PostMapping
     public Product postMethodName(@RequestBody Product product) {
-        
+        producer.producer(product);
+        LOGGER.info("{}", product.getName());
         return product;
     }
     
